d1559fb367b4198a7c01aaae9cc39681d1009aee
jomeltapawan <jomeltapawan35@gmail.com>
2025-09-29 15:10:27 -0700

    Implemented Error handling for command line usage

--- filecopy.cpp
+++ filecopy.cpp
@@ -9,13 +9,19 @@
  #include <unistd.h>
  #include <stdio.h>
  #include <fcntl.h>
+ #include <iostream>
  #include <sys/types.h>
  #include <sys/wait.h>
 
  #define READ_END 0
  #define WRITE_END 1
 
+ using namespace std;
+
  int main(int argc, char *argv[])
  {
-    
+    if (argc != 3) {
+        cout << "Error Usage:" << argv[0] << "(source_file) (destination_file)" << endl;
+        return 0;
+    }
  }
